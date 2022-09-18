ea2361e5c5746211f3bf8fc4ca5ad1b56837fa47
Baron Leonardo <mohamed.a.elmeligy@protonmail.com>
2022-09-18 23:55:43 +0200

    check nvim version for remap

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,4 +2,11 @@
 
 -- nnoremap("<leader>f", "<cmd>Ex<CR>")
 local opts = { noremap = true, silent=true }
-vim.keymap.set({'n', 'v', 'x', 'i'}, '<Leader>f', '<Cmd>Ex<CR>', opts)
+
+if vim.fn.has('nvim-0.7') == 1 then
+    vim.keymap.set({'n', 'v', 'x', 'i'}, '<Leader>f', '<Cmd>Ex<CR>', opts)
+    vim.keymap.set({'n', 'v', 'x', 't'}, '<Leader>t', '<Cmd>ToggleTerm<CR>', opts)
+else
+    vim.api.nvim_set_keymap('n', "<Leader>f", "<cmd>Ex<CR>", opts)
+    vim.api.nvim_set_keymap('n', "<Leader>t", "<cmd>ToggleTerm<CR>", opts)
+end
