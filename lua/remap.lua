e014139f382413037cf289526657e1e89c3813e5
Baron Leonardo <mohamed.a.elmeligy@protonmail.com>
2022-10-09 10:38:06 +0200

    BUG: don't use <leader> in insert mode

--- lua/remap.lua
+++ lua/remap.lua
@@ -4,7 +4,7 @@
 local opts = { noremap = true, silent=true }
 
 if vim.fn.has('nvim-0.7') == 1 then
-    vim.keymap.set({'n', 'v', 'x', 'i'}, '<Leader>f', '<Cmd>Ex<CR>', opts)
+    vim.keymap.set({'n', 'v', 'x'}, '<Leader>f', '<Cmd>Ex<CR>', opts)
     vim.keymap.set({'n', 'v', 'x', 't'}, '<Leader>t', '<Cmd>ToggleTerm<CR>', opts)
 else
     vim.api.nvim_set_keymap('n', "<Leader>f", "<cmd>Ex<CR>", opts)
